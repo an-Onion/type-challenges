f223519b48a251db2730cc5f337423780b78bd88
Onion <congma@outlook.com>
2024-05-26 16:48:53 +0800

    I can't under stand 3188 today

---
+++ src/02946-medium-objectentries.ts
@@ -0,0 +1,24 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface Model {
+  name: string
+  age: number
+  locations: string[] | null
+}
+
+type ModelEntries = ['name', string] | ['age', number] | ['locations', string[] | null]
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ['key', undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ['key', undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined | string }>, ['key', string | undefined]>>,
+]
+
+
+// ============= Your Code Here =============
+type ObjectEntries<T, R extends T = Required<T> > = {
+  [P in keyof R]: [P, R[P] extends undefined ? undefined : R[P]]
+}[keyof T]
