603fbeb133fdbf6e3bdea4ee80386fb941c401bf
Onion <congma@outlook.com>
2024-06-06 22:50:41 +0800

    2 problems in the morning

---
+++ src/05310-medium-join.ts
@@ -0,0 +1,18 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Join<['a', 'p', 'p', 'l', 'e'], '-'>, 'a-p-p-l-e'>>,
+  Expect<Equal<Join<['Hello', 'World'], ' '>, 'Hello World'>>,
+  Expect<Equal<Join<['2', '2', '2'], 1>, '21212'>>,
+  Expect<Equal<Join<['o'], 'u'>, 'o'>>,
+  Expect<Equal<Join<[], 'u'>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type Join<T extends string[], U extends number | string> = T extends [infer F extends string, ...infer R extends string[]]
+  ? `${F}${R extends [] ? '' : U}${Join<R, U>}`
+  : ''
+
+
