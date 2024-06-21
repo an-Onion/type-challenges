85bf96868da479e446210a1206225682a149b600
Onion <congma@outlook.com>
2024-06-21 21:17:03 +0800

    A little complicated

---
+++ src/21104-medium-findall.ts
@@ -0,0 +1,26 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<FindAll<'Collection of TypeScript type challenges', 'Type'>, [14]>>,
+  Expect<Equal<FindAll<'Collection of TypeScript type challenges', 'pe'>, [16, 27]>>,
+  Expect<Equal<FindAll<'Collection of TypeScript type challenges', ''>, []>>,
+  Expect<Equal<FindAll<'', 'Type'>, []>>,
+  Expect<Equal<FindAll<'', ''>, []>>,
+  Expect<Equal<FindAll<'AAAA', 'A'>, [0, 1, 2, 3]>>,
+  Expect<Equal<FindAll<'AAAA', 'AA'>, [0, 1, 2]>>,
+]
+
+
+// ============= Your Code Here =============
+type FindAllHelper<F extends string, U extends string, C extends number[] = [], K extends any[] = []> =
+  F extends `${string}${infer R}`
+  ? F extends `${U}${string}`
+    ? FindAllHelper<R, U, [...C, K['length']], [...K, 0]>
+    : FindAllHelper<R, U, C, [...K, 0]>
+  : C;
+
+
+type FindAll<T extends string, P extends string> = P extends ''
+  ? []
+  : FindAllHelper<T, P>
