bb3cae21e69cb795f2f5d969b7097a4444752752
Onion <congma@outlook.com>
2024-07-03 22:17:32 +0800

    odd is too too easy

---
+++ src/30301-medium-isodd.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<IsOdd<2023>, true>>,
+  Expect<Equal<IsOdd<1453>, true>>,
+  Expect<Equal<IsOdd<1926>, false>>,
+  Expect<Equal<IsOdd<number>, false>>,
+]
+
+
+// ============= Your Code Here =============
+type Odd = 1 | 3 | 5 | 7 | 9
+type IsOdd<T extends number> =  `${T}` extends `${number | ''}${Odd}` ? true : false;
