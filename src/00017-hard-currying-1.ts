8b031dea36e9dbd635c588b282891a16437ca600
Onion <congma@outlook.com>
2024-07-09 21:26:12 +0800

    still a little confused about Currying

---
+++ src/00017-hard-currying-1.ts
@@ -0,0 +1,25 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+const curried1 = Currying((a: string, b: number, c: boolean) => true)
+const curried2 = Currying((a: string, b: number, c: boolean, d: boolean, e: boolean, f: string, g: boolean) => true)
+const curried3 = Currying(() => true)
+
+type cases = [
+  Expect<Equal<
+    typeof curried1, (a: string) => (b: number) => (c: boolean) => true
+  >>,
+  Expect<Equal<
+    typeof curried2, (a: string) => (b: number) => (c: boolean) => (d: boolean) => (e: boolean) => (f: string) => (g: boolean) => true
+  >>,
+  Expect<Equal<typeof curried3, () => true>>,
+]
+
+
+// ============= Your Code Here =============
+declare function Currying<F>(fn: F): Curried<F>
+type Curried<F> = F extends (...args: infer U) => infer C
+  ? U extends [infer K, ...infer Other]
+    ? (arg: K) => Other['length'] extends 0 ? C : Curried<(...args: Other) => C>
+    : () => C
+  : never
