b17781f7a30afe8fe72d7ee0e57dfd8deeb70d88
Onion <congma@outlook.com>
2024-07-24 21:21:03 +0800

    Can not fully understand the unique

---
+++ src/00847-hard-string-join.ts
@@ -0,0 +1,34 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+// Edge cases
+const noCharsOutput = join('-')()
+const oneCharOutput = join('-')('a')
+const noDelimiterOutput = join('')('a', 'b', 'c')
+
+// Regular cases
+const hyphenOutput = join('-')('a', 'b', 'c')
+const hashOutput = join('#')('a', 'b', 'c')
+const twoCharOutput = join('-')('a', 'b')
+const longOutput = join('-')('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+
+type cases = [
+  Expect<Equal<typeof noCharsOutput, ''>>,
+  Expect<Equal<typeof oneCharOutput, 'a'>>,
+  Expect<Equal<typeof noDelimiterOutput, 'abc'>>,
+  Expect<Equal<typeof twoCharOutput, 'a-b'>>,
+  Expect<Equal<typeof hyphenOutput, 'a-b-c'>>,
+  Expect<Equal<typeof hashOutput, 'a#b#c'>>,
+  Expect<Equal<typeof longOutput, 'a-b-c-d-e-f-g-h'>>,
+]
+
+
+// ============= Your Code Here =============
+
+type Join<T extends string, P extends string[]> = P extends [infer A extends string, ...infer B extends string[]] 
+  ? B['length'] extends 0
+    ? A
+    : `${A}${T}${Join<T, B>}`
+  : ''
+
+declare function join<T extends string>(delimiter: T): <P extends string[]>(...parts: P) => Join<T, P>
