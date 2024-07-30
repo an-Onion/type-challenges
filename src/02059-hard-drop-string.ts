db816ae838f0c082abf0cf619e357c3fd7514ce6
Onion <congma@outlook.com>
2024-07-30 21:43:05 +0800

    Who's your Dad

---
+++ src/02059-hard-drop-string.ts
@@ -0,0 +1,25 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<DropString<'butter fly!', ''>, 'butter fly!'>>,
+  Expect<Equal<DropString<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<'butter fly!', 'but'>, 'er fly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'tub'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
+
+
+// ============= Your Code Here =============
+type DropChar<S extends string, R extends string> = S extends `${infer F}${R}${infer L}` 
+  ? `${F}${DropChar<L, R>}` 
+  : S
+
+type DropString<S extends string, R> = R extends `${infer F}${infer R}`
+  ? DropString<DropChar<S, F>, R>
+  : S
