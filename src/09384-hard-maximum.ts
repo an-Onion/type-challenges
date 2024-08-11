38eed0f0617ac01eda64d1259268d2cb67d8164d
Onion <congma@outlook.com>
2024-08-11 15:09:24 +0800

    Maximum is harder than minimum

---
+++ src/09384-hard-maximum.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Maximum<[]>, never>>,
+  Expect<Equal<Maximum<[0, 2, 1]>, 2>>,
+  Expect<Equal<Maximum<[1, 20, 200, 150]>, 200>>,
+];
+
+// ============= Your Code Here =============
+type Maximum<
+  T extends any[],
+  U = T[number],
+  N extends unknown[] = [],
+> = T extends []
+  ? never
+  : Equal<U, N["length"]> extends true
+    ? U
+    : Maximum<T, Exclude<U, N['length']>, [...N, unknown]>;
